fa88429d77070264300d931bfe1e221e948bb795
Fantomos <15447079+Fantomos@users.noreply.github.com>
2021-10-17 17:34:02 +0200

    l

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <main.h>
 
-uint32_t state_rpi = 0x00;
-uint32_t state_pic = 0x00;
-uint32_t batterie = 0;
-uint32_t sleep_hour = 2;
-uint32_t wakeup_hour = 4;
+
+uint32_t state = 0;
+uint32_t battery = 0;
+uint32_t sleep_hour = 19;
+uint32_t wakeup_hour = 7;
+uint32_t error_code = 0;
+uint32_t battery_threshold = 0;
+bool first_cycle = true;
+RTCZero rtc;
+
 
 void receiveI2C(int packetSize)
 {
@@ -20,11 +25,24 @@ void receiveI2C(int packetSize)
       Wire.write(sleep_hour);
       break;
 
-    case REG_EVEIL:
+    case REG_WAKE:
       Wire.write(wakeup_hour);
       break;
 
+    case REG_ERROR:
+      Wire.write(error_code);
+      break;
+
+    case REG_TIME:
+      Wire.write((u_int32_t)rtc.getEpoch());
+      break;
+
+    case REG_STATE:
+      Wire.write(state);
+      break;
+
     default:
+      bitSet(error_code, ERROR_I2C_REG_NOT_FOUND);
       break;
     }
   }
@@ -57,35 +75,170 @@ void receiveI2C(int packetSize)
         sleep_hour = val;
         break;
 
-      case REG_EVEIL:
+      case REG_WAKE:
         wakeup_hour = val;
         break;
 
+      case REG_ERROR:
+        error_code = val;
+        break;
+
+      case REG_STATE:
+        state = val;
+        break;
+
+      case REG_TIME:
+        if(val == 0){
+          setRTCTime(getTimeFromSigfox());
+        }else{
+          setRTCTime(val);
+        }
+        break;
+
+      case REG_DATA:
+        sendDataToSigfox(val);
+        break;
+
       default:
+        bitSet(error_code, ERROR_I2C_REG_NOT_FOUND);
         break;
     }
   }
 }
 
+
+void sendDataToSigfox(uint32_t data){
+  if(!SigFox.begin()){
+      bitSet(error_code, ERROR_SIGFOX_BEGIN);
+  }
+  delay(100);
+  SigFox.beginPacket();
+  SigFox.write(data);
+  int ret = SigFox.endPacket(); 
+  if (ret > 0) {
+    bitSet(error_code, ERROR_SIGFOX_TRANSMIT);
+  }
+  SigFox.end();
+  bitSet(state, FLAG_SIGFOX_TRANSMITTED);
+}
+
+uint32_t getTimeFromSigfox(){
+  if(!SigFox.begin()){
+    bitSet(error_code, ERROR_SIGFOX_BEGIN);
+  }
+  delay(100);
+  SigFox.beginPacket();
+  SigFox.write(0);
+  int ret = SigFox.endPacket(true); 
+  if (ret > 0) {
+    bitSet(error_code, ERROR_SIGFOX_TRANSMIT);
+  }
+
+  uint8_t time_buf[8] = {0,0,0,0,0,0,0,0};
+  uint8_t i = 0;
+  if (SigFox.parsePacket()) {
+    while (SigFox.available()) {
+      time_buf[i] = SigFox.read(); 
+      i++;
+    }
+  }
+  SigFox.end();
+  return (uint32_t) (time_buf[0] << 24 | time_buf[1] << 16 | time_buf[2] << 8 | time_buf[3]);
+}
+
+void setRTCTime(u_int32_t unix_time){
+  time_t t = unix_time;
+  rtc.setTime(hour(t), minute(t), second(t));
+  rtc.setDate(day(t), month(t), year(t)-2000);
+  bitSet(state, FLAG_PIC_TIME_REFRESHED);
+}
+
+void setAlarmForNextCycle(){
+  rtc.detachInterrupt();
+  rtc.attachInterrupt(alarmNextCycle);
+  rtc.setAlarmTime(00, (rtc.getMinutes()+10)%60, 00);
+  rtc.enableAlarm(rtc.MATCH_MMSS);
+  rtc.standbyMode();
+}
+
+void setAlarmForNextDay(){
+  rtc.detachInterrupt();
+  rtc.attachInterrupt(alarmFirstCycle);
+  rtc.setAlarmTime(wakeup_hour, 00, 00);
+  rtc.enableAlarm(rtc.MATCH_HHMMSS);
+  rtc.standbyMode();
+}
+
+void powerUpRPI(){
+  digitalWrite(PIN_POWER_RPI, HIGH);
+  while(bitRead(state, FLAG_RPI_POWER) == 0){};
+}
+
+void powerDownRPI(){
+  while(bitRead(state, FLAG_RPI_POWER) == 1){};
+  delay(1000);
+  digitalWrite(PIN_POWER_RPI, LOW);
+}
+
+void alarmFirstCycle(){
+    bitSet(state, FLAG_FIRST_CYCLE);
+    cycle();
+}
+
+void alarmNextCycle(){
+    bitClear(state, FLAG_FIRST_CYCLE);
+    cycle();
+}
+
+void cycle(){
+  rtc.disableAlarm();
+  if(rtc.getHours() < sleep_hour && rtc.getHours() > wakeup_hour){
+    battery = analogRead(PIN_BATTERY);
+    if(battery > battery_threshold){
+      powerUpRPI();
+      powerDownRPI();
+    }
+    setAlarmForNextCycle();
+  }else{
+    setAlarmForNextDay();
+  }
+
+}
+
+
+// FIRST STARTUP
 void setup()
 {
-  // put your setup code here, to run once:
+  // SERIAL INIT
   Serial.begin(9600);
   while (!Serial){};
 
+  // I2C INIT
   Wire.begin(MKRFOX_ADDR);
   Wire.onReceive(receiveI2C); // register event
 
-  
-  state_rpi = 0; // 0-Eteint 1-Démarré 2-Données compilées 3-Terminé
-  state_pic = 0;
+  // RTC INIT
+  rtc.begin();
 
+  // PIN INIT
+  pinMode(PIN_POWER_RPI, OUTPUT);
 
-  if (!SigFox.begin())
-  {
-    Serial.println("Shield error or not present!");
-    return;
-  }
+  // REGISTER INIT
+  state = 0; // 0-Eteint 1-Démarré 2-Données compilées 3-Terminé
+  battery = 0;
+  battery_threshold = 11;
+  sleep_hour = 19;
+  wakeup_hour = 7;
+  error_code = 0;
+  first_cycle = true;
+
+  setRTCTime(1634293107);
+
+  // RTC INIT
+  rtc.attachInterrupt(alarmFirstCycle);
+  setAlarmForNextCycle();
+
+  rtc.standbyMode();
 }
 
 void loop()
--- src/main.h
+++ src/main.h
@@ -3,17 +3,29 @@
 #include <ArduinoLowPower.h>
 #include <RTCZero.h>
 #include <Wire.h>
+#include <TimeLib.h>
 
 #define MKRFOX_ADDR 0x55
 
 #define REG_TIME 0x01
-#define REG_ALARM 0x02
-#define REG_ALARM_CON 0x03
-#define REG_WIND_DIR 0x04
-#define REG_WIND_SPEED 0x05
-#define REG_STATE_RPI 0x06
-#define REG_STATE_PIC 0x07
+#define REG_STATE 0x07
 #define REG_BATTERY 0x08
 #define REG_SLEEP 0x09
-#define REG_EVEIL 0x10
+#define REG_WAKE 0x10
+#define REG_ERROR 0x11
+#define REG_DATA 0x12
 
+
+#define ERROR_SIGFOX_BEGIN 0
+#define ERROR_SIGFOX_TRANSMIT 1
+#define ERROR_I2C_REG_NOT_FOUND 2
+
+#define FLAG_RPI_POWER 0
+#define FLAG_PIC_TIME_REFRESHED 2
+#define FLAG_FIRST_CYCLE 3
+#define FLAG_RPI_DATA_READY 4
+#define FLAG_SIGFOX_TRANSMITTED 5
+#define FLAG_REQUEST_SHUTDOWN 6
+
+#define PIN_POWER_RPI 1
+#define PIN_BATTERY 1
